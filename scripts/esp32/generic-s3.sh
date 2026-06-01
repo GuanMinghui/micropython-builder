d8136c7b01156a8c0bc628b5e1da1de7a84cc72d
GuanMinghui <144416649+GuanMinghui@users.noreply.github.com>
2026-06-01 13:45:03 +0800

    Disable PSRAM in ESP32_GENERIC_S3 build
    Add macro definition to disable PSRAM in mpconfigboard.h before building.

--- scripts/esp32/generic-s3.sh
+++ scripts/esp32/generic-s3.sh
@@ -8,4 +8,8 @@
 
 source ./scripts/esp32/esp32.sh
 
-build_esp32 "ESP32_GENERIC_S3_4M"
+# 1. 在编译前，直接用 echo 强行在你云端拉下来的 mpconfigboard.h 里追加关闭 PSRAM 的宏定义！
+# 这样既能完美使用官方的配置目录，又能从底层彻底阉割掉检测 PSRAM 的代码！
+echo "#define MICROPY_HW_PSRAM_ENABLE (0)" >> ./micropython/ports/esp32/boards/ESP32_GENERIC_S3/mpconfigboard.h
+
+build_esp32 "ESP32_GENERIC_S3"
